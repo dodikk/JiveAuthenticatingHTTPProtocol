9d9c250370f9aa1d62a047eb7c759a9fb15a29b6
Alexander Dodatko <alexander.dodatko@dataart.com>
2018-07-27 16:15:29 +0300

    "pod install" command consequences
    Pods directory is not included to .gitignore
    So I’m not ignoring these changes and committing them.

--- Pods/Target Support Files/Pods-JiveAuthenticatingHTTPProtocolDemoTests-JiveAuthenticatingHTTPProtocol/Pods-JiveAuthenticatingHTTPProtocolDemoTests-JiveAuthenticatingHTTPProtocol-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JiveAuthenticatingHTTPProtocolDemoTests_JiveAuthenticatingHTTPProtocol : NSObject
-@end
-@implementation PodsDummy_Pods_JiveAuthenticatingHTTPProtocolDemoTests_JiveAuthenticatingHTTPProtocol
-@end
